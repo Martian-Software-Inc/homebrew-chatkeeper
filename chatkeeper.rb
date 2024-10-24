ddec4748d851a2ec6c69398ef5a8315e9df35ac1
Marty Lamb <mlamb@martiansoftware.com>
2024-10-24 08:26:20 -0400

    Updated to beta, more testing.  This would be a lot easier if I had some apple silicon myself.

--- chatkeeper.rb
+++ chatkeeper.rb
@@ -1,14 +1,34 @@
 class Chatkeeper < Formula
     desc "ChatKeeper command-line tool"
     homepage "https://martiansoftware.com/chatkeeper"
-    version "1.0.0"
+    version "1.0.1-beta-1"
   
-    url "https://files.martiansoftware.com/chatkeeper/1.0.0/chatkeeper-1.0.0-macos-x86_64.tar.gz"
-    sha256 "dd4414d62f97c915e0513f15596089a40610f8c08bf3fb4db843379e1e9874e6"
+    url "https://files.martiansoftware.com/chatkeeper/1.0.1-beta-1/chatkeeper-1.0.1-beta-1-macos-x86_64.tar.gz"
+    sha256 "ce061a9d8ce1a74eb7664bf59bae6d8f003d139354cfd72bc8e9f7ade5537880"
   
-    depends_on arch: :x86_64
-  
+    def needs_rosetta
+      <<-EOS
+        **********************************************************************
+        *  On Apple Silicon Macs, ChatKeeper *should* run under Rosetta 2.   *
+        *                                                                    *
+        *  In this beta version, running under Rosetta has not been tested.  *
+        *                                                                    *
+        *  To install Rosetta 2, run:                                        *
+        *    softwareupdate --install-rosetta --agree-to-license             *
+        *                                                                    *
+        *  Please send any feedback, either positive or negative, about      *
+        *  your experience with ChatKeeper under Rosetta to                  *
+        *  chatkeeper@martiansoftware.com.                                   *
+        **********************************************************************
+      EOS
+    end
+
     def install
+      if Hardware::CPU.arm?
+        unless system("/usr/bin/arch", "-x86_64", "/usr/bin/true")
+          odie needs_rosetta
+        end
+      end        
       bin.install "chatkeeper"
     end
   
@@ -16,10 +36,8 @@ class Chatkeeper < Formula
       <<~EOS
         ChatKeeper has been installed!
   
-        On Apple Silicon Macs, ChatKeeper runs under Rosetta 2.
-        To install Rosetta 2, run:
-          softwareupdate --install-rosetta --agree-to-license
-  
+        #{needs_rosetta}
+
         For more information, visit:
         https://martiansoftware.com/chatkeeper
       EOS
@@ -28,7 +46,7 @@ class Chatkeeper < Formula
     test do
       assert_predicate bin/"chatkeeper", :executable?
       output = shell_output("#{bin}/chatkeeper --version")
-      assert_match "ChatKeeper version 1.0.1", output
+      assert_match "ChatKeeper version 1.0.1-beta-1", output
     end
   end
   
