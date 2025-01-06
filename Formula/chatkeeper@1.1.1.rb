7116b462a86982d5d0f8cb4686e826cc11ebc23a
Marty Lamb <mlamb@martiansoftware.com>
2025-01-05 20:26:42 -0500

    releasing v1.1.1

--- Formula/chatkeeper@1.1.1.rb
+++ Formula/chatkeeper@1.1.1.rb
@@ -1,4 +1,4 @@
-class Chatkeeper < Formula
+class ChatkeeperAT111 < Formula
   desc "Convert ChatGPT conversation exports into local Markdown files"
   homepage "https://martiansoftware.com/chatkeeper"
   version "1.1.1"
